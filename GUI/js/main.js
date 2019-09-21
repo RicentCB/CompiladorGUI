d1655f85280c5b62dc8a903896eb0b16ecbb8238
RicentCB <ricardo.cb009@gmail.com>
2019-09-21 08:38:40 -0500

    Script en un nuevo archivo

--- GUI/js/main.js
+++ GUI/js/main.js
@@ -1,9 +1,3 @@
 $(document).ready(function(){
-    let mainTitle = $("h1.main-title");
-    let abtn = $("a.btn");
 
-    abtn.click(function(e){
-        e.preventDefault()
-        mainTitle.html("COMPPP")
-    })
 })
