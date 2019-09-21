1de2517586baa8d875910c8c8d2e035a83f9004a
RicentCB <ricardo.cb009@gmail.com>
2019-09-21 13:02:57 -0500

    Plantilla Finalizada

---
+++ GUI/Engine/test.py
@@ -0,0 +1,6 @@
+import sys
+
+stringIn = sys.argv[1]
+
+print("Automata basico de: "+stringIn+" creado")
+sys.stdout.flush()
--- GUI/js/canvas.js
+++ GUI/js/canvas.js
@@ -1,11 +1,59 @@
+const R = 25;
+const LINEW = 50;
+const DEC_TXT = 12
+const INI_SP = 20
+const Y_INI = 100
+const COLOR_CNVS = "#555";
 
+//Funcion que dibuja una flecha dadas coordenadas x,y de orgine y destino
+function canvas_arrow(context, fromx, fromy, tox, toy) {
+    var headlen = 10; // length of head in pixels
+    var dx = tox - fromx;
+    var dy = toy - fromy;
+    var angle = Math.atan2(dy, dx);
+    context.moveTo(fromx, fromy);
+    context.lineTo(tox, toy);
+    context.lineTo(tox - headlen * Math.cos(angle - Math.PI / 6), toy - headlen * Math.sin(angle - Math.PI / 6));
+    context.moveTo(tox, toy);
+    context.lineTo(tox - headlen * Math.cos(angle + Math.PI / 6), toy - headlen * Math.sin(angle + Math.PI / 6));
+}
 $(document).ready(function(){
 
-    // var canvas = document.getElementById("goodCanvas1");
-    // var ctx = canvas.getContext("2d");
-    // ctx.fillText("Hello ARIA World", canvas.width/2, canvas.height/2);
-    // ctx.arc(50, 100, 50, 0, Math.PI*2);
-    // ctx.stroke();
+    
+
+    var canvas = document.getElementById("goodCanvas1");
+    var ctx = canvas.getContext("2d");
+    
+    ctx.strokeStyle = COLOR_CNVS;
+    ctx.lineWidth = 2;
+    ctx.font = "20px Quicksand";
+    ctx.fillStyle = COLOR_CNVS;
+
+    ctx.beginPath();
+    ctx.arc((R+LINEW+INI_SP), Y_INI, R, 0, 2 * Math.PI, false);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.fillText("q0", R+LINEW+INI_SP-DEC_TXT, Y_INI+5);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(INI_SP+(R+LINEW)+((2*R)+LINEW), Y_INI, R, 0, 2 * Math.PI, false);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.fillText("q1", INI_SP+(R+LINEW)+((2*R)+LINEW)-DEC_TXT, Y_INI+5);
+    ctx.fill();
+
+    ctx.beginPath();
+    canvas_arrow(ctx, INI_SP, Y_INI, INI_SP+LINEW, Y_INI);
+    canvas_arrow(ctx, INI_SP+(2*R+LINEW), Y_INI, INI_SP+(2*R+LINEW)+1*(LINEW), Y_INI);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.fillText("a-b", (INI_SP+LINEW+R)+(LINEW+R)/2, Y_INI-10);
+    ctx.fill();
+
 
     var addBasicAFDBtn = $("a.btn.add-btn#basicAFD");
     var addRangeAFDBtn = $("a.btn.add-btn#rangeAFD")
@@ -46,14 +94,6 @@ $(document).ready(function(){
     });
 
     /*
-    //Boton Agregar Automata
-    
-    
-    //AFD Basico
-    addBasicAFDBtn.click(function(e){
-        
-        
-    });
     //AFD de Rango
     addRangeAFDBtn.click(function(e){
         e.preventDefault();
