8cce9380b8d4821d1be47e17fc15dfda28b3f8d8
RicentCB <ricardo.cb009@gmail.com>
2019-10-18 22:24:52 -0500

    Prueba de Serializacion

---
+++ GUI/Engine/serial.py
@@ -0,0 +1,18 @@
+import pickle
+
+# data = []
+
+file_name = "/home/ricardo/ESCOM/5 Semestre/Compiladores/CompiladorGUI/GUI/Engine/AFNS.txt"
+# fileObject = open(file_name, 'wb')
+
+#Escribir el objeto
+# pickle.dump(data,fileObject)
+# fileObject.close()
+
+#Leer El mensaje
+fileObjectRead = open(file_name, 'rb')
+read = pickle.load(fileObjectRead) 
+if len(read) > 0:
+    for elem in read:
+        print(elem)
+
