20065a845bfce9104ab1a6e4fb137433a757e28a
RicentCB <ricardo.cb009@gmail.com>
2019-10-18 22:23:32 -0500

    Inicializacion de Serializacion AFN

--- GUI/js/main.js
+++ GUI/js/main.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-    
+    const {PythonShell} = require("python-shell");
+    var path = require("path");
+    //Funcion que Incializa el arreglo de Automatas
+    var options = {
+        mode: 'json',
+        pythonOptions: ['-u'],
+        scriptPath:  path.join(__dirname, 'Engine/'),
+        pythonPath: '/usr/bin/python3',         //Cambiar la ruta de acuerdo al sistema
+        args: ["AFN","Inicializar"]
+    };
+
+    let ans = new PythonShell('main.py', options);
+    //Creamos el automata
+    ans.on('message',function(jsonString){
+        console.log("Incilizado")
+    });
+
 })
