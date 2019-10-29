d98b9134969f43176549eab97715f1562665283f
RicentCB <ricardo.cb009@gmail.com>
2019-10-28 20:15:23 -0600

    Creacion de Tabla de gramatica

--- GUI/js/grammar.js
+++ GUI/js/grammar.js
@@ -1,4 +1,17 @@
+var path = require("path");
+
+    var optionsPython = {
+        mode: 'json',
+        pythonOptions: ['-u'],
+        scriptPath: path.join(__dirname, 'Engine/'),
+        pythonPath: '/usr/bin/python3',         //Cambiar la ruta de acuerdo al sistema
+        args: []
+    };
+
 $(document).ready(function(){
+
+    const { PythonShell } = require("python-shell");
+
     let JSONTable = {
         Head :["SIM1", "SIM2", "SIM3", "SIM4", "SIM5", "SIM6"],
         Body : [
@@ -7,40 +20,63 @@ $(document).ready(function(){
             ["1","2","3", "4","5", "6"],
         ]
     }
-    let containerTable = $("main#grammar .create-table-result#grammar-ll1");
-    //Construir la tabla con json generado
-    let strTable = "<table>";
-        //Agregar Encabezado
-        strTable += "<thead><tr>";
-        for (let i = 0; i < JSONTable.Head.length; i++) {
-            strTable += "<td>"+JSONTable.Head[i]+"</td>";
-        }
-        strTable += "</tr></thead>";
-        //Agregar cuerpo
-        strTable += "<tbody>";
-        for (let i = 0; i < JSONTable.Body.length; i++) {
-            strTable += "<tr>";
-            for (let j = 0; j < JSONTable.Body[0].length; j++) {
-                strTable += "<td>"+JSONTable.Body[i][j]+"</td>";                
-            }
-            strTable += "</tr>";
-            
-        }
-        strTable += "</tbody>";
-        strTable += "</table>";
-    //Insertar el Nodo
-    containerTable.html("");
-    containerTable.append(strTable);
+    
 
     let btnGetFile = $("main#grammar #file-input-grammar");
+    let containerTable = $("main#grammar .create-table-result#grammar-ll1");
     
     btnGetFile.change(function (e){
-
-        let fileType = e.target.files[0].type;
-        if(fileType != "text/plain"){
+        let fileIn = e.target.files[0];
+        if(fileIn.type != "text/plain"){
             swal.fire("Error", "Solo archivos (*.txt)", "error");
         }else{
-            //NO hay error en el tipo de archivos
+            //NO hay error en el tipo de archivo
+            console.log()
+            //Llamamos al metodo de Python
+            optionsPython.args = ["Grammar", "Path", fileIn.path]
+
+            let resultPython = new PythonShell('main.py', optionsPython);
+            resultPython.on('message', function (jsonString) {
+                //Insertar JSON en Arreglo
+                console.log(jsonString);
+                if (jsonString["message"] == true){
+                    Swal.fire("Gramatica Acpetada", "", "success");
+                    //Creamos la tabla
+                    
+                    //Construir la tabla con json generado
+                    let strTable = "<table>";
+                    //Agregar Encabezado
+                    strTable += "<thead><tr>";
+                    for (let i = 0; i < jsonString.Head.length; i++) {
+                        strTable += "<td>"+jsonString.Head[i]+"</td>";
+                    }
+                    strTable += "</tr></thead>";
+                    //Agregar cuerpo
+                    strTable += "<tbody>";
+                    for (let i = 0; i < jsonString.Body.length; i++) {
+                        strTable += "<tr>";
+                        for (let j = 0; j < jsonString.Body[0].length; j++) {
+                            if(jsonString.Body[i][j] == " "){
+                                strTable += "<td class='td-white'></td>"
+                            }else{
+                                strTable += "<td>"+jsonString.Body[i][j]+"</td>";                
+                            }
+                        }
+                        strTable += "</tr>";
+                        
+                    }
+                    strTable += "</tbody>";
+                    strTable += "</table>";
+                    //Insertar el Nodo
+                    containerTable.html("");
+                    containerTable.append(strTable);
+                    
+                }
+                else{
+                    swal.fire("Error en la gramatica", "", "error");
+                    console.log(jsonString["message"])
+                }
+            });
         }
         
     })
