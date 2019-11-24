54963e3062d318945cc9080dd493628e72723ba9
RicentCB <ricardo.cb009@gmail.com>
2019-11-24 17:55:51 -0600

    Interprete

--- GUI/Engine/compiler/interpreter.py
+++ GUI/Engine/compiler/interpreter.py
@@ -1,119 +1,8 @@
 from sly import Lexer
 from sly import Parser
 
-class BasicLexer(Lexer):
-    tokens = { NAME, NUMBER, STRING, IF, THEN, ELSE, FOR, FUN, TO, ARROW, EQEQ }
-    ignore = '\t '
-
-    literals = { '=', '+', '-', '/', '*', '(', ')', ',', ';' }
-
-    # Define tokens
-    IF = r'IF'
-    THEN = r'THEN'
-    ELSE = r'ELSE'
-    FOR = r'FOR'
-    FUN = r'FUN'
-    TO = r'TO'
-    ARROW = r'->'
-    NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
-    STRING = r'\".*?\"'
-
-    EQEQ = r'=='
-
-    @_(r'\d+')
-    def NUMBER(self, t):
-        t.value = int(t.value)
-        return t
-
-    @_(r'#.*')
-    def COMMENT(self, t):
-        pass
-
-    @_(r'\n+')
-    def newline(self,t ):
-        self.lineno = t.value.count('\n')
-
-class BasicParser(Parser):
-    tokens = BasicLexer.tokens
-
-    precedence = (
-        ('left', '+', '-'),
-        ('left', '*', '/'),
-        ('right', 'UMINUS'),
-        )
-
-    def __init__(self):
-        self.env = { }
-
-    @_('')
-    def statement(self, p):
-        pass
-
-    @_('FOR var_assign TO expr THEN statement')
-    def statement(self, p):
-        return ('for_loop', ('for_loop_setup', p.var_assign, p.expr), p.statement)
-
-    @_('IF condition THEN statement ELSE statement')
-    def statement(self, p):
-        return ('if_stmt', p.condition, ('branch', p.statement0, p.statement1))
-
-    @_('FUN NAME "(" ")" ARROW statement')
-    def statement(self, p):
-        return ('fun_def', p.NAME, p.statement)
-
-    @_('NAME "(" ")"')
-    def statement(self, p):
-        return ('fun_call', p.NAME)
-
-    @_('expr EQEQ expr')
-    def condition(self, p):
-        return ('condition_eqeq', p.expr0, p.expr1)
-
-    @_('var_assign')
-    def statement(self, p):
-        return p.var_assign
-
-    @_('NAME "=" expr')
-    def var_assign(self, p):
-        return ('var_assign', p.NAME, p.expr)
-
-    @_('NAME "=" STRING')
-    def var_assign(self, p):
-        return ('var_assign', p.NAME, p.STRING)
-
-    @_('expr')
-    def statement(self, p):
-        return (p.expr)
-
-    @_('expr "+" expr')
-    def expr(self, p):
-        return ('add', p.expr0, p.expr1)
-
-    @_('expr "-" expr')
-    def expr(self, p):
-        return ('sub', p.expr0, p.expr1)
-
-    @_('expr "*" expr')
-    def expr(self, p):
-        return ('mul', p.expr0, p.expr1)
-
-    @_('expr "/" expr')
-    def expr(self, p):
-        return ('div', p.expr0, p.expr1)
-
-    @_('"-" expr %prec UMINUS')
-    def expr(self, p):
-        return p.expr
-
-    @_('NAME')
-    def expr(self, p):
-        return ('var', p.NAME)
-
-    @_('NUMBER')
-    def expr(self, p):
-        return ('num', p.NUMBER)
-
-
+from parser_lexer import Hoc5Parser
+from parser_lexer import Hoc5Lexer
 
 class BasicExecute:
 
@@ -184,7 +73,7 @@ class BasicExecute:
             try:
                 return self.env[node[1]]
             except LookupError:
-                print("Undefined variable '"+node[1]+"' found!")
+                print("Variable indefinida '"+node[1]+"' no se econtro!")
                 return 0
 
         if node[0] == 'for_loop':
@@ -206,14 +95,21 @@ class BasicExecute:
 
 
 if __name__ == '__main__':
-    lexer = BasicLexer()
-    parser = BasicParser()
+    lexer = Hoc5Lexer()
+    parser = Hoc5Parser()
     env = {}
-    while True:
-        try:
-            text = input('basic > ')
-        except EOFError:
-            break
-        if text:
-            tree = parser.parse(lexer.tokenize(text))
-            BasicExecute(tree, env)
+    fileProgram = open('GUI/Engine/compiler/programEx.txt', 'r')
+    # flines = fileProgram.readlines()
+    text = fileProgram.read()
+    print(text)
+    # for line in flines:
+    #     tree = parser.parse(lexer.tokenize(text))
+    #     BasicExecute(tree, env)
+    # while True:
+    # try:
+    #     text = input('basic > ')
+    # except EOFError:
+    #     print("ERROR")
+    if text:
+        tree = parser.parse(lexer.tokenize(text))
+        BasicExecute(tree, env)
